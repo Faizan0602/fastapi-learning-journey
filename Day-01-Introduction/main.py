b49a8f0ac63d38c2ad7a45f6e3bdbabfbb9e98fa
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-08-26 20:24:13 +0530

    basic fast api structure

--- Day-01-Introduction/main.py
+++ Day-01-Introduction/main.py
@@ -3,7 +3,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+#home route
 @app.get("/")
 
 def home():
-    return{"message" : "hello from venv"}
+    return{"message" : "This is home page"}
+
+#about route
+@app.get("/about")
+
+def about():
+    return{"message" : "This is about page"}
+
+#user route
+@app.get("/users")
+
+def users():
+    return{
+        "users" : ["Faizan" , "Alex" , "Ahmad"]
+    }
