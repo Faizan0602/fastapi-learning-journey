276431d7624f8416edea5ba03465ce3df0d97eda
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-08-29 15:52:19 +0530

    path parameter

--- Day-01-Introduction/main.py
+++ Day-01-Introduction/main.py
@@ -1,24 +1,37 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# #home route
+# @app.get("/")
+
+# def home():
+#     return{"message" : "This is home page"}
+
+# #about route
+# @app.get("/about")
+
+# def about():
+#     return{"message" : "This is about page"}
+
+# #user route
+# @app.get("/users")
+
+# def users():
+#     return{
+#         "users" : ["Faizan" , "Alex" , "Ahmad"]
+#     }
+    
+# dynamic routes and path parameters : 
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+#users route
+@app.get("/users/{user_id}")   #path parameter
 
-#home route
-@app.get("/")
+def get_user(user_id:int):
+    return{"user_id": user_id}
 
-def home():
-    return{"message" : "This is home page"}
-
-#about route
-@app.get("/about")
-
-def about():
-    return{"message" : "This is about page"}
-
-#user route
-@app.get("/users")
-
-def users():
-    return{
-        "users" : ["Faizan" , "Alex" , "Ahmad"]
-    }
